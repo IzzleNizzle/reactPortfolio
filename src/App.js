63f69b9f4616216356bcb920579f80b46a3c4b02
Iz P <pehisaac@gmail.com>
2018-07-04 14:12:22 -0600

    Made some updates.

--- src/App.js
+++ src/App.js
@@ -20,7 +20,13 @@ class App extends Component {
           <div className="row container">
             <img src="/assets/AmberandI.JPG" id="profile" alt="Me" />
             <h3 className="header">Hi! I'm Isaac</h3>
-            <p className="grey-text text-darken-3 lighten-3">I'm a Web Developer. I currently work with the MERN Stack.</p>
+            <p className="grey-text text-darken-3 lighten-3">I'm a Web Developer currently working with the MERN Stack.
+            <br/>
+            <br/>
+            I love the diversity of people. Coding makes me happy.
+            <br/>
+            <br/>
+            In my free time you can find me peddling my bike, riding at the skatepark, or with my laptop coding up a storm.</p>
           </div>
         </div>
         <Parallax imageSrc="./assets/ipadchessmatch.jpg" />
@@ -36,14 +42,20 @@ class App extends Component {
 
           links={
             <ul>
-              <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+              <li>
+              <a href="https://github.com/IzzleNizzle" className='logo'><span ><img className='logo'src="assets/githubicon.png" alt="#" /></span> GitHub</a>
+              </li>
+              <li>
+              <a href="https://www.linkedin.com/in/-isaacp/" className='logo'><span ><img className='logo'src="assets/linkedinicon.png" alt="#" /></span> LinkedIn</a>
+              </li>
+              <li>
+              <a href="https://stackoverflow.com/users/8822068/isaac?tab=profile" className='logo'><span ><img className='logo'src="assets/stackoverflowicon.png" alt="#" /></span> Stack Overflow</a>
+              </li>
             </ul>
           }
           className='example'
         >
-          <h5 className="white-text">Thanks for reading!</h5>
-          <p className="grey-text text-lighten-4">Feel free to connect with me below.</p>
-          <p className="grey-text text-lighten-4">Some icons here</p>
+          <h5 className="white-text">Let's connect!</h5>
         </Footer>
 
       </div>
